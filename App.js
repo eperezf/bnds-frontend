b095af8a38439dc1158f2801fce82fbac031d515
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-06-15 00:05:35 -0400

    DynamoDB Tables declaration

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <StatusBar style="default" />
+      <View style={styles.phoneBar}></View>
+    </ScrollView>
   );
 }
 
@@ -18,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  phoneBar: {
+    flex: 1,
+    backgroundColor: '#e6e6e6',
+    height: 50
+  }
 });
