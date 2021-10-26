cf8b767afb0ddea070fbfb995e0de05d38d83331
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-10-26 19:05:00 -0300

    Result test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,9 @@
 <script>
   import { variables } from '$lib/variables';
-  import { fade } from 'svelte/transition';
+  import { slide } from 'svelte/transition';
   let darkMode = false;
-
+  let mobileMenu = false;
+  let operator;
   function toggleDarkMode(){
     if (darkMode) {
       darkMode = false;
@@ -10,109 +11,90 @@
       darkMode = true;
     }
   }
+
+  function toggleMobileMenu(){
+    if (mobileMenu) {
+      mobileMenu = false
+    } else {
+      mobileMenu = true;
+    }
+  }
 </script>
-<main class="{darkMode? "dark" : ""}">
-  <nav class="bg-yellow-500 sticky md:m-2 md:rounded-lg md:shadow dark:bg-gray-800 dark:text-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between h-16">
-        <div class="flex items-center">
-          <div class="flex-shrink-0">
-            <img class="h-10" src="/logo.png" alt="Workflow">
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-10 flex items-baseline space-x-4">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="#" class="bg-yellow-600 text-gray-800 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Inicio</a>
-
-              <a href="#" class="text-gray-900 hover:bg-yellow-600 hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">Nosotros</a>
-
-              <a href="#" class="text-gray-900 hover:bg-yellow-600 hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">API (Pronto!)</a>
-            </div>
-          </div>
-        </div>
-        <div class="hidden md:block">
-          <div class="ml-4 flex items-center md:ml-6">
-            {#if !darkMode}
-              <button on:click={toggleDarkMode}>
-                <i class="fas fa-sun"></i>
-              </button>
-            {:else}
-              <button on:click={toggleDarkMode}>
-                <i class="fas fa-moon"></i>
-              </button>
-            {/if}
-          </div>
-        </div>
-        <div class="-mr-2 flex md:hidden">
-          <!-- Mobile menu button -->
-          <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <!--
-              Heroicon name: outline/menu
-
-              Menu open: "hidden", Menu closed: "block"
-            -->
-            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            <!--
-              Heroicon name: outline/x
-
-              Menu open: "block", Menu closed: "hidden"
-            -->
-            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
+<main class="{darkMode? "dark" : ""} h-screen">
+  <!-- Mobile Menu -->
+  <div class="absolute md:hidden inset-x-0 top-0">
+    <div class="h-20 bg-emerald-500 shadow-lg grid grid-cols-2 p-4">
+      <div class="justify-self-start self-center">
+        <img src="/logo.png" class="h-12"/>
+      </div>
+      <div class="justify-self-end self-center">
+        <button class="border-2 rounded-md border-emerald-600 h-10 w-10" on:click={toggleMobileMenu}>
+          <i class="fas fa-bars"></i>
+        </button>
       </div>
     </div>
-
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="md:hidden" id="mobile-menu">
-      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
-
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Reports</a>
+    {#if mobileMenu}
+      <div class="bg-emerald-600 grid p-4 shadow divide-solid divide-y divide-emerald-700" transition:slide>
+        <button class="p-2">
+          <i class="fas fa-home"></i>
+          Inicio
+        </button>
+        <button class="p-2">
+          <i class="fas fa-info-circle"></i>
+          Nosotros
+        </button>
+        <button class="p-2">
+          <i class="fas fa-question-circle"></i>
+          ¿Cuál es la variante de mi teléfono?
+        </button>
+        <button class="p-2">
+          <i class="fas fa-code"></i>
+          API (pronto!)
+        </button>
       </div>
-      <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-5">
-          <div class="flex-shrink-0">
-            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-            <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-          </div>
-          <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-            <span class="sr-only">View notifications</span>
-            <!-- Heroicon name: outline/bell -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-3 px-2 space-y-1">
-          <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
-
-          <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
-
-          <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
-        </div>
+    {/if}
+  </div>
+  <!-- Desktop Menu -->
+  <div class="hidden md:block absolute inset-x-0 top-0 p-2">
+    <div class="h-20 bg-emerald-500 shadow-lg p-4 rounded-lg">
+      <div class="flex max-w-screen-lg mx-auto">
+        <img src="/logo.png" class="h-12 mr-2"/>
+        <button class="p-2">
+          <i class="fas fa-home"></i>
+          Inicio
+        </button>
+        <button class="p-2">
+          <i class="fas fa-info-circle"></i>
+          Nosotros
+        </button>
+        <button class="p-2">
+          <i class="fas fa-question-circle"></i>
+          ¿Cuál es la variante de mi teléfono?
+        </button>
+        <button class="p-2">
+          <i class="fas fa-code"></i>
+          API (pronto!)
+        </button>
       </div>
     </div>
-  </nav>
-
-  <div class="flex flex-wrap justify-center content-center">
-    <div class="h-48 bg-yellow-500 p-3">
-      Selecciona tu operadora:
+  </div>
+  <!--Body-->
+  <div class="pt-20 h-full grid grid-cols-1 items-center content-center justify-center max-w-screen-sm mx-auto">
+    <div class="text-center mx-auto">
+      <p class="text-xl mb-4 text-center">Revisa si tu teléfono es compatible</p>
+      <p class="text-md mb-4 mx-2 text-center">Busca el modelo de tu teléfono y la operadora que quieres usar
+    </div>
+    <div class="bg-emerald-500 p-4 m-4 rounded-lg grid shadow-lg">
+      <label for="operator" class="text-center">Selecciona tu operadora:</label>
+      <select name="operator" id="operator" bind:value={operator} class="rounded-lg mb-4 mt-2">
+        <option>Claro</option>
+        <option>Entel</option>
+        <option>Movistar</option>
+        <option>Virgin Mobile</option>
+      </select>
+      <label for="phone" class="text-center">Busca tu teléfono:</label>
+      <input name="phone" id="phone" type="search" class="rounded-lg mt-2"/>
+      <button class="mt-4 bg-emerald-400 mx-auto p-2 rounded-lg shadow">Verificar</button>
     </div>
   </div>
 </main>
---
+++ src/routes/result.svelte
@@ -0,0 +1,30 @@
+<script>
+let rollOut = false;
+function toggleRollOut(){
+  if (rollOut) {
+    rollOut = false;
+  } else {
+    rollOut = true;
+  }
+}
+</script>
+<main class="bg-gray-200 h-screen">
+  <div class="max-w-sm p-4">
+    <div class="rounded-2xl shadow p-6 bg-white grid grid-cols-1 place-items-center overflow-hidden transition-all" on:click={toggleRollOut}>
+      <div class="text-center font-bold text-xl text-gray-600">
+        Apple iPhone 12 A1234
+      </div>
+      <img src="https://static-dev.bnds.cl/phones/P00ttV.png" class="max-h-80 mx-auto border-solid border-2 border-blue-200 bg-blue-100 rounded-2xl my-7"/>
+      <div class="my-4">
+        <a href="https://pisapapeles.net/review-iphone-12" class=" text-gray-600 hover:text-blue-500 transition-all hover:bg-blue-100 p-2 rounded-lg">Leer Review</a>
+      </div>
+      <p class="my-4 text-gray-600">Otras variantes:</p>
+      <div class="flex gap-2 text-blue-600 flex-wrap justify-center">
+        <p class="p-1 bg-blue-200 rounded-lg">A1234</p>
+        <p class="p-1 bg-blue-200 rounded-lg">A1234</p>
+        <p class="p-1 bg-blue-200 rounded-lg">A1234</p>
+        <p class="p-1 bg-blue-200 rounded-lg">A1234</p>
+      </div>
+    </div>
+  </div>
+</main>
