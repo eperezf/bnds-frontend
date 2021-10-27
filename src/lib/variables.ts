e6c39b85cbec77c5c4b97de5e054152c07de75e2
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-10-27 10:13:57 -0300

    Added ©, fixed return url for password change

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,5 +1,6 @@
 export const variables = {
   apiEndpoint: import.meta.env.VITE_API_ENDPOINT,
   nodeEnv: import.meta.env.VITE_NODE_ENV,
-  staticEndpoint: import.meta.env.VITE_STATIC_ENDPOINT
+  staticEndpoint: import.meta.env.VITE_STATIC_ENDPOINT,
+  returnUrl: import.meta.env.VITE_RETURN_URL
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,14 @@
-<main class="h-screen bg-gray-100 dark:bg-gray-800">
-  <div class="flex justify-center content-center h-screen items-center">
+<svelte:head>
+  <meta name="apple-mobile-web-app-capable" content="yes">
+</svelte:head>
+<main class="bg-gray-100 dark:bg-gray-800">
+  <div class="flex justify-center content-center items-center flex-grow h-screen">
     <div class="w-48 bg-white p-4 rounded-2xl shadow">
       <img src="bnds_dark.png" alt="logo" />
-      <p class="text-center">Pronto...</p>
+      <p class="text-center text-gray-600">Pronto...</p>
     </div>
+  </div>
+  <div class="absolute bottom-0 inset-x-0 text-center text-black dark:text-white mb-8">
+    © 2021 Pisapapeles Networks Ltda.
   </div>
 </main>
